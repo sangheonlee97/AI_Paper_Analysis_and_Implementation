f1463cf371ab31cb77a0e2b3e42671a939b951e2
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 17:46:30 +0900

    Transformer_TF / decoder
    decoder layer 쌓기

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -136,3 +136,25 @@ def decoder_layer(dff, d_model, num_heads, dropout, name='decoder_layer'):
     
     return tf.keras.Model(inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask], outputs=outputs, name=name)
 
+def decoder(vocab_size, num_layers, dff, d_model, num_heads, dropout, name='decoder'):
+    inputs = tf.keras.Input(shape=(None, ), name='inputs')
+    enc_outputs = tf.keras.Input(shape=(None, d_model), name='encoder_outputs')
+    
+    # 디코더는 룩어헤드 마스크와 패딩마스크 둘 다 사용
+    look_ahead_mask = tf.keras.Input(shape=(1, None, None), name='look_ahead_mask')
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+    
+    # positional encoding + Dropout
+    embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+    embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+    embeddings = positional_encoding(vocab_size, d_model)(embeddings)
+    outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+    
+    # 디코더 레이어 쌓기
+    for i in range(num_layers):
+        outputs = decoder_layer(dff=dff, d_model=d_model, num_heads=num_heads, dropout=dropout, name='decoder_layer_{}'.format(i),)(inputs=[outputs, enc_outputs, look_ahead_mask, padding_mask])
+        
+    return tf.keras.Model(inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask], outputs=outputs, name=name)
+
+
+        
