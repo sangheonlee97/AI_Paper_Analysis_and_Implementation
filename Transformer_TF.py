9e55d599c1bde213e8e7a75d5c255c6770e3f6b4
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 17:00:57 +0900

    Transformer_TF / encoder
    encoder layer 쌓아서 encoder 만들기

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -85,3 +85,21 @@ def encoder_layer(dff, d_model, num_heads, dropout, name='encoder_layer'):
     
     return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
 
+def encoder(vocab_size, num_layers, dff, d_model, num_heads, dropout, name='encoder'):
+    inputs = tf.keras.Input(shape=(None, ), name='inputs')
+    
+    # 인코더는 패딩 마스크 사용
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+    
+    # positional encoding + dropout
+    embeddings = tf.keras.layers.Embedding(vocab_size, d_model)(inputs)
+    embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+    embeddings = positional_encoding(vocab_size, d_model)(embeddings)
+    outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+    
+    # 인코더 레이어 쌓기
+    for i in range(num_layers):
+        outputs = encoder_layer(dff=dff, d_model=d_model, num_heads=num_heads, dropout=dropout, name='encoder_layer_{}'.format(i),)([outputs, padding_mask])
+        
+    return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
