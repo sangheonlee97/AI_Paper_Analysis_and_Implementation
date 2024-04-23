e95dd2d3fe376bf060c0f174c1abcb56c2427c0f
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 17:39:35 +0900

    Transformer_TF / decoder_layer
    디코더는 세 개의 서브층으로 구성된다. 첫번째, 두번째 층은 Multi Head Attention(멀티 헤드 어텐션)으로 이루어 져있는데, 위에서 말했듯이 첫번째 서브층에서는 현재 예측하고자 하는 단어 이후의 단어들을 참조하지 않게 하기 위해 mask의 인자값으로 look ahead mask(룩 어헤드 마스크)가 사용된다. 두번째 서브층은 mask의 인자값으로 encoder layer와 동일하게 padding mask가 사용된다. 그리고 세번째 서브층은 역시나 피드 포워드 신경망으로 이루어 진다.

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -103,3 +103,36 @@ def encoder(vocab_size, num_layers, dff, d_model, num_heads, dropout, name='enco
         
     return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
 
+def decoder_layer(dff, d_model, num_heads, dropout, name='decoder_layer'):
+    inputs = tf.keras.Input(shape=(None, d_model), name='inputs')
+    enc_outputs = tf.keras.Input(shape=(None, d_model), name='encoder_outputs')
+    
+    # look ahead mask
+    look_ahead_mask = tf.keras.Input(shape=(1, None, None), name='look_ahead_mask')
+    
+    # padding mask
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+    
+    # multi head attention( masked attention )
+    attention1 = MultiHeadAttention(d_model=d_model, num_heads=num_heads, name='attention_1')(inputs={'query' : inputs, 'key' : inputs, 'value' : inputs, 'mask' : look_ahead_mask})
+    
+    # ADD + layernormalization
+    attention1 = tf.keras.layers.LayerNormalization(epsilion=1e-6)(attention1 + inputs)
+    
+    # multi head attention( decoder - encoder attention )
+    attention2 = MultiHeadAttention(d_model, num_heads, name='attention_2')(inputs={'query' : attention1, 'key' : enc_outputs, 'value' : enc_outputs, 'mask' : padding_mask})
+    
+    # dropout + ADD + layernormalization
+    attention2 = tf.keras.layers.Dropout(rate=dropout)(attention2)
+    attention2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention2 + attention1)
+    
+    # feed forward
+    outputs = tf.keras.layers.Dense(dff, activation='relu')(attention2)
+    outputs = tf.keras.layers.Dense(d_model)(outputs)
+    
+    # dropout + ADD + norm
+    outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+    outputs = tf.keras.layers.LayerNormalization(epsilon=1e-6)(outputs + attention2)
+    
+    return tf.keras.Model(inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask], outputs=outputs, name=name)
+
