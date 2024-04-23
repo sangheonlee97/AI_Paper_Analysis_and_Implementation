da1b1f45beccd4dda83b9342f408066049fbf7db
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 17:55:08 +0900

    Transformer_TF / Transformer
    완성

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -156,5 +156,36 @@ def decoder(vocab_size, num_layers, dff, d_model, num_heads, dropout, name='deco
         
     return tf.keras.Model(inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask], outputs=outputs, name=name)
 
+def transformer(vocab_size, num_layers, dff, d_model, num_heads, dropout, name="transformer"):
+    # 인코더의 입력
+    inputs = tf.keras.Input(shape=(None, ), name="inputs")
 
+    # 디코더의 입력
+    dec_inputs = tf.keras.Input(shape=(None, ), name="dec_inputs")
+
+    # 인코더의 패딩 마스크
+    enc_padding_mask = tf.keras.layers.Lambda(
+        create_padding_mask, output_shape=(1, 1, None),
+        name='enc_padding_mask')(inputs)
+
+    # 디코더의 룩어헤드 마스크(첫번째 서브층)
+    look_ahead_mask = tf.keras.layers.Lambda(
+        create_look_ahead_mask, output_shape=(1, None, None),
+        name='look_ahead_mask')(dec_inputs)
+
+    # 디코더의 패딩 마스크(두번째 서브층)
+    dec_padding_mask = tf.keras.layers.Lambda(
+        create_padding_mask, output_shape=(1, 1, None),
+        name='dec_padding_mask')(inputs)
+
+    # 인코더의 출력은 enc_outputs. 디코더로 전달된다.
+    enc_outputs = encoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff, d_model=d_model, num_heads=num_heads, dropout=dropout,)(inputs=[inputs, enc_padding_mask]) # 인코더의 입력은 입력 문장과 패딩 마스크
+
+    # 디코더의 출력은 dec_outputs. 출력층으로 전달된다.
+    dec_outputs = decoder(vocab_size=vocab_size, num_layers=num_layers, dff=dff, d_model=d_model, num_heads=num_heads, dropout=dropout,)(inputs=[dec_inputs, enc_outputs, look_ahead_mask, dec_padding_mask])
+
+    # 다음 단어 예측을 위한 출력층
+    outputs = tf.keras.layers.Dense(units=vocab_size, name="outputs")(dec_outputs)
+
+    return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
         
