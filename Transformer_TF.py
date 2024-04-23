2c35bb9474cead6752a69edff9622599b0c1779a
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 16:47:16 +0900

    Transformer_TF / encoder_layer
    인코더 레이어는 두개의 서브층으로 이루어 지는데 멀티 헤드 어텐션과 피드 포워드 신경망으로 이루어 져있다. 그런데 인코더의 입력 문장에 패딩 토큰이 있을 수 있어 이를 언텐션 하지 않도록 해주기 위해 Multi Head Attention의 Padding Mask(패딩 마스크)를 사용해준다.

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -59,3 +59,29 @@ def create_look_ahead_mask(X):
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
     padding_mask = create_padding_mask(X)
     return tf.maximum(look_ahead_mask, padding_mask)
+
+def encoder_layer(dff, d_model, num_heads, dropout, name='encoder_layer'):
+    inputs = tf.keras.Input(shape=(None, d_model), name='inputs')
+    
+    # 인코더는 패딩 마스크 사용
+    padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+    
+    # 멀티 헤드 어텐션
+    attention = MultiHeadAttention(d_model, num_heads, name='attention')({
+        'query' : inputs, 'key' : inputs, 'value' : inputs, 'mask' : padding_mask
+        })
+    
+    # DropOut + ADD + LayerNormalization
+    attention = tf.keras.layers.Dropout(rate=dropout)(attention)
+    attention = tf.keras.layers.LayerNormalization(epsilon=1e-6)(inputs + attention)
+    
+    # 피드 포워드
+    outputs = tf.keras.layers.Dense(units=dff, activation='relu')(attention)
+    outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+    
+    # DropOut + ADD + LayerNormalization
+    outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+    outputs = tf.keras.layers.LayerNormalization(epsilon=1e-6)(attention + outputs)
+    
+    return tf.keras.Model(inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
