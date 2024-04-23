9c71959b3e3d3d959462b7e5bf1b50d44c1286e6
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-23 16:08:58 +0900

    Transformer_TF / MultiHeadAttention

--- Transformer_TF.py
+++ Transformer_TF.py
@@ -13,3 +13,32 @@ def scaled_dot_product_attention(query, key, value, mask=None):
     attention_weights = tf.nn.softmax(scaled_attention_logits, axis = -1)
     output = tf.matmul(attention_weights, value)
     return output, attention_weights
+
+class MultiHeadAttention(layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+        
+        assert d_model % self.num_heads == 0
+        
+        self.depth = d_model // self.num_heads
+        self.wq = layers.Dense(d_model)
+        self.wk = layers.Dense(d_model)
+        self.wv = layers.Dense(d_model)
+        self.dense = layers.Dense(d_model)
+        
+    def split_heads(self, X, batch_size):
+        X = tf.reshape(X, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(X, perm=[0, 2, 1, 3])
+    
+    def call(self, query, key, value, mask):
+        batch_size = tf.shape(query)[0]
+        query = self.split_heads(self.wq(query), batch_size)
+        key = self.split_heads(self.wk(key), batch_size)
+        value = self.split_heads(self.wv(value), batch_size)
+        
+        output, attention_weights = scaled_dot_product_attention(query, key, value, mask)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(output, (batch_size, -1, self.d_model))
+        return self.dense(concat_attention)
