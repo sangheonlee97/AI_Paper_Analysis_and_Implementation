16740c0937e1031ea0b5f3a03224b5083b9dfee3
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-25 16:04:13 +0900

    Transformer_Torch / ScaleDotProductAttention

--- Transformer_Torch.py
+++ Transformer_Torch.py
@@ -21,3 +21,24 @@ class PositionalEncoding(nn.Module):
         batch_size, seq_len = x.size()
         return self.encoding[:seq_len, :]
 
+
+class ScaleDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaleDotProductAttention, self).__init__()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, mask=None, e=1e-12):
+        batch_size, head, length, d_tensor = k.size()
+
+        k_t = k.transpose(2, 3)
+        score = (q @ k_t) / math.sqrt(d_tensor)
+
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -10000)
+
+        score = self.softmax(score)
+
+        v = score @ v
+
+        return v, score
+
