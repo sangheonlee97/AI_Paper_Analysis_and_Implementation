20fa66687389d0ecaa3f33f85cd9d11a6806d0e1
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-04-25 16:17:47 +0900

    Transformer_Torch / EncoderLayer

--- Transformer_Torch.py
+++ Transformer_Torch.py
@@ -112,3 +112,27 @@ class PositionwiseFeedForward(nn.Module):
 
         return x
 
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(EncoderLayer, self).__init__()
+        self.attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.norm1 = LayerNorm(d_model=d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.norm2 = LayerNorm(d_model=d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+    def forward(self, x, src_mask):
+        _x = x
+        x = self.attention(q=x, k=x, v=x, mask=src_mask)
+        x = self.dropout1(x)
+        x = self.norm1(x + _x)
+
+        -x = x
+        x = self.ffn(x)
+        x = self.dropout2(x)
+        x = self.norm2(x + _x)
+
+        return x
+
