ba8a5ae2c4732ef745d0d7237973075ddb385b89
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-06 11:59:28 +0900

    Mask-RCNN / 데이터셋

--- Mask_RCNN.py
+++ Mask_RCNN.py
@@ -28,3 +28,56 @@ cv2.rectangle(sample, pt1, pt2, color=(255,0,0), thickness=2)
 plt.imshow(sample)
 plt.show()
 
+class CarDataset(Dataset):
+    def __init__(self, df, image_dir, transforms=None):
+        super().__init__()
+        
+        self.image_ids = df["image"].unique() # all image filenames
+        self.df = df
+        self.image_dir = image_dir # dir to image files
+        self.transforms = transforms
+
+    def __getitem__(self, idx: int):
+        image_id = self.image_ids[idx]
+        records = self.df[self.df["image"] == image_id]
+        image = cv2.imread(f"{self.image_dir}/{image_id}", cv2.IMREAD_COLOR)
+        heights, widths = image.shape[:2]
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+        image = torch.tensor(image)
+        image = image.permute(2,0,1)
+        
+        
+        boxes = records[["xmin", "ymin", "xmax", "ymax"]].values
+        
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        area = torch.as_tensor(area, dtype=torch.float32)
+        
+        masks = []
+        for box in boxes:
+            mask = np.zeros([int(heights), int(widths)], np.uint8)
+            masks.append(cv2.rectangle(mask, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), 1, -1))
+
+        masks = torch.tensor(masks, dtype=torch.uint8)
+
+        # class가 1종류이기 때문에 label은 1로만 지정
+        labels = torch.ones((records.shape[0]), dtype=torch.int64)
+        
+        target = {}
+        target["boxes"] = torch.tensor(boxes)
+        target["labels"] = labels
+        target['masks'] = masks
+        target["image_id"] = torch.tensor([idx])
+        target["area"] = area
+
+
+        if self.transforms:
+            sample = {"image": image, "boxes": target["boxes"], "labels": labels}
+            sample = self.transforms(**sample)
+            image = sample["image"]
+            target["boxes"] = torch.stack(tuple(map(torch.tensor, zip(*sample["boxes"])))).permute(1, 0)
+
+        return image, target
+
+    def __len__(self):
+        return self.image_ids.shape[0]
