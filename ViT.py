a777ef01ccf98ca3ed527e1f56df349ff20873f5
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-22 12:00:15 +0900

    ViT / MSA
    멀티헤드 셀프 어텐션. 헤드 개수로 토큰을 나누고 셀프 어텐션을 한 후에 각 패치별 차원들에 대해 소프트맥스를 해주어 어텐션 스코어를 구한다.

--- ViT.py
+++ ViT.py
@@ -33,8 +33,38 @@ class EmbeddingLayer(nn.Module):
         return z
 
 
+class MSA(nn.Module):
+    def __init__(self, dim=192, num_heads=12, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        assert dim % num_heads == 0, 'dim should be divisible by num_heads'
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
 if __name__ == '__main__':
     img = torch.randn([2, 3, 32, 32])
     embedding = EmbeddingLayer(in_chans=3, embed_dim=192, img_size=32, patch_size=4)
     z = embedding(img)
-    print(z.size()) 
+    msa = MSA()
+    print(msa(z).size()) 
