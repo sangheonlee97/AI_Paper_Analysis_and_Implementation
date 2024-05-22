a57f33c9b0f2b38315f78b7d33e91f90aad1eff2
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-22 12:17:16 +0900

    ViT / transformer encoder block
    임베딩된 벡터를 MSA를 거친 후에 MLP 해주는 인코더 블럭

--- ViT.py
+++ ViT.py
@@ -82,7 +82,24 @@ class MLP(nn.Module):
         return x
     
     
+class Block(nn.Module):
 
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False,
+                 drop=0., attn_drop=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm,
+                 ):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.norm2 = norm_layer(dim)  
+        self.attn = MSA(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
+        self.mlp = MLP(in_features=dim, hidden_features=int(dim * mlp_ratio), act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
+    
+    
+    
 
 if __name__ == '__main__':
     img = torch.randn([2, 3, 32, 32])
