b38e6bc999a9368a076c51c0c591bc0bcf807541
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-22 12:15:54 +0900

    ViT / MLP

--- ViT.py
+++ ViT.py
@@ -62,6 +62,28 @@ class MSA(nn.Module):
         return x
 
 
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features, act_layer=nn.GELU, bias=True, drop=0.):
+        super().__init__()
+        out_features = in_features
+
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop)
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+        self.drop2 = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
+    
+    
+
+
 if __name__ == '__main__':
     img = torch.randn([2, 3, 32, 32])
     embedding = EmbeddingLayer(in_chans=3, embed_dim=192, img_size=32, patch_size=4)
