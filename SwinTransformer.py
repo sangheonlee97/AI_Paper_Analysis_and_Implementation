86eea4ec373a0b35a6976d92298b6e2ced0aba67
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-22 16:31:29 +0900

    SwinTransformer / W_MSA
    window로 잘라서 window 내부의 시퀀스끼리 어텐션 처리하는 클래스

--- SwinTransformer.py
+++ SwinTransformer.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import math
 
 class PatchPartition(nn.Module):
     def __init__(self,
@@ -17,4 +18,61 @@ class PatchPartition(nn.Module):
         x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
         x = self.norm(x)
         return x
+    
+class W_MSA(nn.Module):
+    def __init__(self,
+                 dim, num_heads, head_dim=None, window_size=7,
+                 qkv_bias=True, attn_drop=0., proj_drop=0.):
+
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        head_dim = head_dim or dim // num_heads
+        attn_dim = head_dim * num_heads
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, attn_dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(attn_dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        # setting
+        B, L, C = x.shape
+        ws = self.window_size
+        w = h = int(math.sqrt(L))
+        h_ = int(h // ws)
+        w_ = int(w // ws)
+
+        # [B, 3136, C]
+        # ----------- efficient batch computation for shifted configuration -----------
+        x = x.view(B, h, w, C)                        # [B, H, W, C]
+        x = x.view(B, h_, ws, w_, ws, C)              # [0, 1, 2, 3, 4, 5 ] -> [0, 1, 3, 2, 4, 5 ] - idx
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous()  # [B, 8, 7, 8, 7, 96] -> [B, 8, 8, 7, 7, 96]
+        x = x.view(B * h_ * w_, ws * ws, C)           # [B' = B x 8 x 8],   -> [B'         49, 96]
+
+        # ------------------------------ attention ------------------------------
+        B_, N, C = x.shape                            # [B_, 49, 96]
+        qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, -1).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)
+
+        q = q * self.scale
+        attn = (q @ k.transpose(-2, -1))
+
+        attn = self.softmax(attn)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B_, N, -1)
+        x = self.proj(x)
+        x = self.proj_drop(x)                                                           # [B_, 49, 96]
+
+        # ---------- make multi-batch tensor original batch tensor ----------v
+        x = x.view(B, h_, w_, ws, ws, C)              # [B, 8, 8, 7, 7, 96]
+        x = x.permute(0, 1, 3, 2, 4, 5).contiguous()  # [B, 8, 7, 8, 7, 96]
+        x = x.view(B, h, w, -1)               # (roll)  [B, 56, 56, 96]
+        x = x.view(B, h * w, C)                       # [B, 56, 56, 96]
+        return x
+    
     
