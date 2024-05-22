fa4f17cfb05badd5865b8a164fd55a1f4cb2ff08
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-05-22 12:17:56 +0900

    ViT / 완성

--- ViT.py
+++ ViT.py
@@ -99,7 +99,37 @@ class Block(nn.Module):
         return x
     
     
+
+class ViT(nn.Module):
+    def __init__(self, img_size=32, patch_size=4, in_chans=3, num_classes=10, embed_dim=192, depth=12,
+                 num_heads=12, mlp_ratio=2., qkv_bias=False, drop_rate=0., attn_drop_rate=0.):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = nn.LayerNorm
+        act_layer = nn.GELU
+
+        self.patch_embed = EmbeddingLayer(in_chans, embed_dim, img_size, patch_size)
+        self.blocks = nn.Sequential(*[
+            Block(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, drop=drop_rate,
+                attn_drop=attn_drop_rate, norm_layer=norm_layer, act_layer=act_layer)
+            for i in range(depth)])
+        
+        # final norm
+        self.norm = norm_layer(embed_dim)
+
+        # Classifier head(s)
+        self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward(self, x):
+        x = self.patch_embed(x)
+        x = self.blocks(x)
+        x = self.norm(x)
+        x = self.head(x)[:, 0]
+        return x
     
+
 
 if __name__ == '__main__':
     img = torch.randn([2, 3, 32, 32])
